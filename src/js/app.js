74fe83d40b662098175a02159fdacb575085d90f
Student <student@localhoct>
2020-01-06 22:05:50 -0800

    increated cashback

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,16 @@
-const regularPurchases = 300000;
-const regularPurchasesPrecent = 0.01;
-let cashback = regularPurchases * regularPurchasesPrecent;
+// const regularPurchases = 300000;
+// const regularPurchasesPrecent = 0.01;
+// let cashback = regularPurchases * regularPurchasesPrecent;
+// console.log(cashback);
+// if (cashback > 3000) {
+//     cashback = 3000; 
+// }
+
+const increasedPurchases = 10000;
+const increasedPurchasesPrecent = 0.05;
+
+const specialPurchases = 50000;
+const specialPurchasesPrecent = 0.3;
+
+let cashback = increasedPurchases * increasedPurchasesPrecent + specialPurchases * specialPurchasesPrecent;
 console.log(cashback);
-if (cashback > 3000) {
-    cashback = 3000; 
-}
